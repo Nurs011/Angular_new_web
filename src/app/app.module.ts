9646569722a405db77520b6a6acbba66bc6f7328
Angular_new_web <kanzharbaev011@gmail.com>
2021-04-29 07:51:31 +0600

    endka

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { KidsPageComponent } from './kids-page/kids-page.component';
 import { MenPageComponent } from './men-page/men-page.component';
 import { WomenPageComponent } from './women-page/women-page.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import {MycustompipePipe} from './Pipes/my.pipe';
 import {ProductService} from './services/product.service';
 import {LoggingService} from './services/logging.service';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+
+
 
 
 
@@ -35,8 +37,10 @@ import { LoginPageComponent } from './login-page/login-page.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [LoggingService, ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
